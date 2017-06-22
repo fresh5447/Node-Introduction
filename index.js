8f75aad311ea496897596a6d1b442de8905805b4
fresh5447 <doug@kosmojo.com>
2017-06-22 12:53:01 -0600

    update readme

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ app.get('/greeting', function(req, res){
   res.send("Woohoo you found the greeting endpoint!")
 });
 
+// :name -> means it is a req parameter
+app.get('/greeting/:name', function(req, res){
+  var yourName = req.params.name;
+  res.send("nice to meet you " + yourName);
+});
+
+//Challenge: define a reponse that can send back your favorite color
+app.get('/color/:color', function(req, res){
+  var yourColor = req.params.color;
+  res.send("nice to meet you " + yourName);
+});
+
+
+
 // Challenge: Define a salutations endpoint
 app.get('/salutations', function(req, res){
   res.send("Thank you Mr. Node 🤓")
 });
-
 
 app.listen(3000, function(){
   console.log('Listening on port 3000');
