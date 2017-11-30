001d131f0ceb855fcf4337f7349a91706f71001d
fresh5447 <doug@kosmojo.com>
2017-11-29 19:31:36 -0700

    add more examples

--- in-to-node/app.js
+++ in-to-node/app.js
@@ -2,15 +2,62 @@
 // import http from 'http'
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 // app.HTTP_METHOD('__URL__', (request, reponse) => {
 //   // HANDLE CLIENT REQUEST
 //   // SERVER SENDS RESPONSE
 // })
 
+app.post('/api/simple', (req, res) => {
+  // req.body is all the form info
+  console.log('REQUEST BODY', req.body)
+  res.json(req.body)
+})
+
+app.post('/api/animals', (req, res) => {
+  // const color = req.body.color
+  // const type = req.body.type
+  // const age = req.body.age
+  // const name = req.body.name
+
+  const {color, type, age, name} = req.body
+  // exact same as code above
+  // const animal = { color: color, type: type, age: age, name: name }
+  const animal = { color, type, age, name }
+
+  // res.json({ msg: 'Animal Created', animal: animal })
+  res.json({ msg: 'Animal Created', animal })
+
+
+})
+
+// API Endpoint to POST a shape
+// A shape has a color, sides, and name
+// IF the shape has more than 4 we get an error msg
+// ELSE we get a success msg and the shape..
+const allShapes = []
+
+app.post('/api/shapes', (req, res) => {
+  const {color, name, sides} = req.body
+  const shape = {color, name, sides}
+
+  if(shape.sides > 4) {
+    res.json({msg: `CAN NOT HAVE ${shape.sides} sidesrs`})
+  } else {
+    allShapes.push(shape)
+    res.json({msg: 'SHAPE ADDED TO DB', data: allShapes})
+  }
+
+})
+
+
 // Test in Postman @ localhost:4000/hello-world
 // Make sure it is a GET method
-
 // GET EXAMPLES
 app.get('/hello-world', (req, res) => {
   res.send('you found the hello world endpoint')
